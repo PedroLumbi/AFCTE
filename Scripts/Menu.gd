a81c3412de8b63d77351f4258763295ef38d05ed
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-05-01 19:38:54 -0600

    primer commit

---
+++ Scripts/Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func NuevoJuego():
+	#Pasar a la primera escena del juego borrando el progreso anterior
+	pass
+
+func Continuar():
+	#Cargar la escena que haya estado en el ultimo punto de guardado
+	pass
+
+func Controles():
+	#Pasar a la escena que contiene los controles WASD y mouse
+	pass
+
+func Salir():
+	get_tree().quit()
