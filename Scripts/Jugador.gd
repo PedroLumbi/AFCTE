340c4691f91bae9416b12d66fc13962a79e9bff3
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-05-03 09:26:19 -0600

    añadido jugador, añadidos los sprites del jugador, añadido el script de movimiento sin probar utilizando move_and_slide

--- Scripts/Jugador.gd
+++ Scripts/Jugador.gd
@@ -1,4 +1,20 @@
-extends Node
+extends KinematicBody2D
+var VELOCIDAD = 100
 
-func _ready():
-	pass
+#Proceso fisico que se sincroniza con el procesador
+func _physics_process(_delta):
+	var movimiento = Vector2()#vector de movimiento
+	
+	#animacion se ejecuta dentro de estos listenner
+	if Input.is_action_pressed("ui_up"):
+		movimiento.y -= 1
+	if Input.is_action_pressed("ui_left"):
+		movimiento.x += 1
+	if Input.is_action_pressed("ui_right"):
+		movimiento.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		movimiento.y += 1 
+	
+	#sincroniza los ticks del procesador con la velocidad de movimiento
+	#Maquillando la latencia interna
+	print(move_and_slide(movimiento))
