232731bed79744b5f2c41e80a29d5b35ce4fbaa8
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-05-03 14:07:59 -0600

    corregida la escena del jugador, añadidos tileSet al mapa, añadidas colisiones, añadida correccion a los creditos, el escenario general ya tiene componentes

--- Scripts/Creditos.gd
+++ Scripts/Creditos.gd
@@ -9,10 +9,15 @@ func _ready():
 func timer():
 	var item = dialogo.pop_front()
 	if item != null:
-		$Label.set_text(item)
+		$Label.text += item+"\n"
 
 func cargarDialogo():
 	var archivo = File.new()
 	archivo.open("res://Dialogos/Creditos.json", archivo.READ)
 	while (!archivo.eof_reached()):
 		dialogo.append(archivo.get_line())
+
+
+func vovlerAlMenuPrincipal():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Escenas/MenuPrincipal.tscn")
--- Scripts/Jugador.gd
+++ Scripts/Jugador.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-var VELOCIDAD = 100
+var VELOCIDAD = 200
 
 #Proceso fisico que se sincroniza con el procesador
 func _physics_process(_delta):
@@ -7,14 +7,11 @@ func _physics_process(_delta):
 	
 	#animacion se ejecuta dentro de estos listenner
 	if Input.is_action_pressed("ui_up"):
-		movimiento.y -= 1
+		movimiento.y -= VELOCIDAD
 	if Input.is_action_pressed("ui_left"):
-		movimiento.x += 1
+		movimiento.x -= VELOCIDAD
 	if Input.is_action_pressed("ui_right"):
-		movimiento.x -= 1
+		movimiento.x += VELOCIDAD
 	if Input.is_action_pressed("ui_down"):
-		movimiento.y += 1 
-	
-	#sincroniza los ticks del procesador con la velocidad de movimiento
-	#Maquillando la latencia interna
-	print(move_and_slide(movimiento))
+		movimiento.y += VELOCIDAD
+	move_and_slide(movimiento)
