f39db779b5336bd00580c5060b41dfb91dcfecf9
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-05-06 16:51:03 -0600

    creado un npc con esquizofrenia severa, añadido a la escena principal, esta muy loco, se recomienda tratarlo con cariño

---
+++ Scripts/NpcDeMovimientoAleatorio.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+export var velocidad = 200
+var random = RandomNumberGenerator.new()
+var movimiento = Vector2()
+func _ready():
+	$temporizador.start()
+	pass
+func _physics_process(_delta):
+	move_and_slide(movimiento)
+	pass
+
+func temporizador():
+	movimiento = Vector2()
+	random.randomize()
+	var resultado = random.randi_range(1, 6)#dado de 6 caras
+	
+	if(resultado > 3):
+		print(resultado)
+		#se mueve
+		random.randomize()
+		resultado = random.randi_range(1,4)#tira dado de 4 caras
+		print(resultado)
+		if(resultado == 1):
+			movimiento.x += velocidad
+		elif(resultado == 2):
+			movimiento.x -= velocidad
+		elif(resultado == 3):
+			movimiento.y += velocidad
+		elif(resultado == 4):
+			movimiento.y -= velocidad
+		
+			
+		pass
+	
+	#se ejecuta cada 2 segundos en FPS de la maquina
+	pass
+
+func huir():
+	#cuando se termine la conversación, el npc huirá
+	#buscará el borde de la camara mas cercano y será borrado de la escena
+	#haciendo que sin importar victoria o derrota
+	#se de la diferencia entre delincuente y NPC generico
+	pass
