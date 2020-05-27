2f10bcec4fa0d36eb5cb13875fbfcb52c3af6696
EmmanuelCVC <64994528+EmmanuelCVC@users.noreply.github.com>
2020-05-27 03:25:00 -0600

    Se lee y guarda texto local
    En la escena de inicio main aparece metodos de leer y guardar texto , con posible utilidad de guardar datos

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,8 +1,35 @@
 extends Control
 
 
+onready var archivo = 'res://texto.txt'
+onready var contador = 0
 
+func cargarArchivo(archivo):
+	var f = File.new()
+	var err = f.open(archivo, File.READ)
+	if err != OK:
+		printerr("Error", err)
+		return ""
+	var texto = f.get_line()
+	contador = int(texto)+1
+	f.close()
+	return texto
+	
+func guardarArchivo(texto, archivo):
+	var f = File.new()
+	var err = f.open(archivo, File.WRITE)
+	if err != OK:
+		printerr("Error", err)
+		return
+	f.store_string(texto)
+	f.close()
+	
 func _ready():
+	
+	print(cargarArchivo(archivo))
+	
+	
+	guardarArchivo(str(contador), archivo)
 	
 	var escena11  = global.escena1.instance()
 	
